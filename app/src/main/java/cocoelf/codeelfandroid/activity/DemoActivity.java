a1d4016f0f50f9d11f9adc140c2f6baaa18e0279
Green-Cherry <2423964104@qq.com>
2018-02-28 14:08:45 +0800

    增加语音样例

---
+++ app/src/main/java/cocoelf/codeelfandroid/activity/DemoActivity.java
@@ -0,0 +1,336 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license.
+ * //
+ * Project Oxford: http://ProjectOxford.ai
+ * //
+ * ProjectOxford SDK GitHub:
+ * https://github.com/Microsoft/ProjectOxford-ClientSDK
+ * //
+ * Copyright (c) Microsoft Corporation
+ * All rights reserved.
+ * //
+ * MIT License:
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * //
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * //
+ * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package cocoelf.codeelfandroid.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.microsoft.bing.speech.SpeechClientStatus;
+import com.microsoft.cognitiveservices.speechrecognition.DataRecognitionClient;
+import com.microsoft.cognitiveservices.speechrecognition.ISpeechRecognitionServerEvents;
+import com.microsoft.cognitiveservices.speechrecognition.MicrophoneRecognitionClient;
+import com.microsoft.cognitiveservices.speechrecognition.RecognitionResult;
+import com.microsoft.cognitiveservices.speechrecognition.RecognitionStatus;
+import com.microsoft.cognitiveservices.speechrecognition.SpeechRecognitionMode;
+import com.microsoft.cognitiveservices.speechrecognition.SpeechRecognitionServiceFactory;
+
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+import cocoelf.codeelfandroid.R;
+
+public class DemoActivity extends Activity implements ISpeechRecognitionServerEvents {
+    int m_waitSeconds = 0;
+    DataRecognitionClient dataClient = null;
+    MicrophoneRecognitionClient micClient = null;
+    FinalResponseStatus isReceivedResponse = FinalResponseStatus.NotReceived;
+    EditText _logText;
+    RadioGroup _radioGroup;
+    Button _buttonSelectMode;
+    Button _startButton;
+
+    public enum FinalResponseStatus {NotReceived, OK, Timeout}
+
+    public String getPrimaryKey() {
+        return this.getString(R.string.primaryKey);
+    }
+
+    private String getLuisAppId() {
+        return this.getString(R.string.luisAppID);
+    }
+
+    private String getLuisSubscriptionID() {
+        return this.getString(R.string.luisSubscriptionID);
+    }
+
+    private Boolean getUseMicrophone() {
+        int id = this._radioGroup.getCheckedRadioButtonId();
+        return id == R.id.micDictationRadioButton ;
+    }
+
+    /**
+     * Gets the current speech recognition mode.
+     *
+     * @return The speech recognition mode.
+     */
+    private SpeechRecognitionMode getMode() {
+        int id = this._radioGroup.getCheckedRadioButtonId();
+        if (id == R.id.micDictationRadioButton ) {
+            return SpeechRecognitionMode.LongDictation;
+        }
+
+        return SpeechRecognitionMode.ShortPhrase;
+    }
+
+    /**
+     * Gets the default locale.
+     *
+     * @return The default locale.
+     */
+    private String getDefaultLocale() {
+        return "zh-cn";
+    }
+
+    /**
+     * Gets the Cognitive Service Authentication Uri.
+     *
+     * @return The Cognitive Service Authentication Uri.  Empty if the global default is to be used.
+     */
+    private String getAuthenticationUri() {
+        return this.getString(R.string.authenticationUri);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+        this._logText = (EditText) findViewById(R.id.editText1);
+        this._radioGroup = (RadioGroup) findViewById(R.id.groupMode);
+        this._buttonSelectMode = (Button) findViewById(R.id.buttonSelectMode);
+        this._startButton = (Button) findViewById(R.id.buttonss);
+
+        if (getString(R.string.primaryKey).startsWith("Please")) {
+            new AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.add_subscription_key_tip_title))
+                    .setMessage(getString(R.string.add_subscription_key_tip))
+                    .setCancelable(false)
+                    .show();
+        }
+
+        // setup the buttons
+        final DemoActivity This = this;
+        this._startButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                This.StartButton_Click(arg0);
+            }
+        });
+
+        this._buttonSelectMode.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                This.ShowMenu(This._radioGroup.getVisibility() == View.INVISIBLE);
+            }
+        });
+
+        this._radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup rGroup, int checkedId) {
+                This.RadioButton_Click(rGroup, checkedId);
+            }
+        });
+
+        this.ShowMenu(true);
+    }
+
+    private void ShowMenu(boolean show) {
+        if (show) {
+            this._radioGroup.setVisibility(View.VISIBLE);
+            this._logText.setVisibility(View.INVISIBLE);
+        } else {
+            this._radioGroup.setVisibility(View.INVISIBLE);
+            this._logText.setText("");
+            this._logText.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Handles the Click event of the _startButton control.
+     */
+    private void StartButton_Click(View arg0) {
+        this._startButton.setEnabled(false);
+        this._radioGroup.setEnabled(false);
+
+        this.m_waitSeconds = this.getMode() == SpeechRecognitionMode.ShortPhrase ? 20 : 200;
+
+        this.ShowMenu(false);
+
+        this.LogRecognitionStart();
+
+        if (this.getUseMicrophone()) {
+            if (this.micClient == null) {
+
+                this.micClient = SpeechRecognitionServiceFactory.createMicrophoneClient(
+                        this,
+                        this.getMode(),
+                        this.getDefaultLocale(),
+                        this,
+                        this.getPrimaryKey());
+
+
+                this.micClient.setAuthenticationUri(this.getAuthenticationUri());
+            }
+
+            this.micClient.startMicAndRecognition();
+        }
+
+    }
+
+    /**
+     * Logs the recognition start.
+     */
+    private void LogRecognitionStart() {
+        String recoSource="";
+        if (this.getUseMicrophone()) {
+            recoSource = "microphone";
+        }
+
+        this.WriteLine("\n--- Start speech recognition using " + recoSource + " with " + this.getMode() + " mode in " + this.getDefaultLocale() + " language ----\n\n");
+    }
+
+    public void onFinalResponseReceived(final RecognitionResult response) {
+        boolean isFinalDicationMessage = this.getMode() == SpeechRecognitionMode.LongDictation &&
+                (response.RecognitionStatus == RecognitionStatus.EndOfDictation ||
+                        response.RecognitionStatus == RecognitionStatus.DictationEndSilenceTimeout);
+        if (null != this.micClient && this.getUseMicrophone() && ((this.getMode() == SpeechRecognitionMode.ShortPhrase) || isFinalDicationMessage)) {
+            // we got the final result, so it we can end the mic reco.  No need to do this
+            // for dataReco, since we already called endAudio() on it as soon as we were done
+            // sending all the data.
+            this.micClient.endMicAndRecognition();
+        }
+
+        if (isFinalDicationMessage) {
+            this._startButton.setEnabled(true);
+            this.isReceivedResponse = FinalResponseStatus.OK;
+        }
+
+        if (!isFinalDicationMessage) {
+            this.WriteLine("********* Final n-BEST Results *********");
+            for (int i = 0; i < response.Results.length; i++) {
+                this.WriteLine("[" + i + "]" + " Confidence=" + response.Results[i].Confidence +
+                        " Text=\"" + response.Results[i].DisplayText + "\"");
+
+            }
+            this.micClient.endMicAndRecognition();
+            this.WriteLine();
+        }
+    }
+
+    /**
+     * Called when a final response is received and its intent is parsed
+     */
+    public void onIntentReceived(final String payload) {
+        this.WriteLine("--- Intent received by onIntentReceived() ---");
+        this.WriteLine(payload);
+        this.WriteLine();
+    }
+
+    public void onPartialResponseReceived(final String response) {
+        this.WriteLine("--- Partial result received by onPartialResponseReceived() ---");
+        this.WriteLine(response);
+        this.WriteLine();
+    }
+
+    public void onError(final int errorCode, final String response) {
+        this._startButton.setEnabled(true);
+        this.WriteLine("--- Error received by onError() ---");
+        this.WriteLine("Error code: " + SpeechClientStatus.fromInt(errorCode) + " " + errorCode);
+        this.WriteLine("Error text: " + response);
+        this.WriteLine();
+    }
+
+    /**
+     * Called when the microphone status has changed.
+     *
+     * @param recording The current recording state
+     */
+    public void onAudioEvent(boolean recording) {
+        this.WriteLine("--- Microphone status change received by onAudioEvent() ---");
+        this.WriteLine("********* Microphone status: " + recording + " *********");
+        if (recording) {
+            this.WriteLine("Please start speaking.");
+        }
+
+        WriteLine();
+        if (!recording) {
+            this.micClient.endMicAndRecognition();
+            this._startButton.setEnabled(true);
+        }
+    }
+
+    /**
+     * Writes the line.
+     */
+    private void WriteLine() {
+        this.WriteLine("");
+    }
+
+    /**
+     * Writes the line.
+     *
+     * @param text The line to write.
+     */
+    private void WriteLine(String text) {
+        this._logText.append(text + "\n");
+    }
+
+    /**
+     * Handles the Click event of the RadioButton control.
+     *
+     * @param rGroup    The radio grouping.
+     * @param checkedId The checkedId.
+     */
+    private void RadioButton_Click(RadioGroup rGroup, int checkedId) {
+        // Reset everything
+        if (this.micClient != null) {
+            this.micClient.endMicAndRecognition();
+            try {
+                this.micClient.finalize();
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+            this.micClient = null;
+        }
+
+        if (this.dataClient != null) {
+            try {
+                this.dataClient.finalize();
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+            this.dataClient = null;
+        }
+
+        this.ShowMenu(false);
+        this._startButton.setEnabled(true);
+    }
+
+}
