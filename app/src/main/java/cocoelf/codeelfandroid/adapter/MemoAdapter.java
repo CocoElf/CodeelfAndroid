e7282fe724ee16c3cb0e405e146f21ec0ab4dcfc
151250148 <151250148@smail.nju.edu.cn>
2018-03-08 21:20:57 +0800

    添加数据

--- app/src/main/java/cocoelf/codeelfandroid/adapter/MemoAdapter.java
+++ app/src/main/java/cocoelf/codeelfandroid/adapter/MemoAdapter.java
@@ -20,6 +20,7 @@ import org.w3c.dom.Text;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import cocoelf.codeelfandroid.R;
@@ -58,31 +59,48 @@ public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.ViewHolder> im
         return viewHolder;
     }
 
+    private String geneKeywords(List<String> keywords){
+        StringBuilder stringBuilder = new StringBuilder();
+        int i;
+        for(i =0;i<keywords.size()-1;i++){
+            stringBuilder.append(keywords.get(i)+" | ");
+        }
+        stringBuilder.append(keywords.get(i));
+        if(stringBuilder.length()>40){
+            return stringBuilder.substring(0,40);
+        }else {
+            return stringBuilder.toString();
+        }
+    }
+
+    private String formatDate(Date date){
+        return new SimpleDateFormat("yyyy/MM/ss").format(date);
+    }
+
     @Override
     public void onBindViewHolder(MemoAdapter.ViewHolder holder, int position) {
         MemoModel memoModel = memoModelList.get(position);
-        SpannableString string = new SpannableString(memoModel.getTitle());
-        string.setSpan(new ForegroundColorSpan(Color.parseColor("#F55D54")),0,memoModel.getType().length()+2,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        holder.memoItemName.setText(string);
-        SpannableString das = new SpannableString(memoModel.getFormatDate()+" - "+memoModel.getSnippet());
-        das.setSpan(new ForegroundColorSpan(Color.parseColor("#333333")),0,memoModel.getFormatDate().length()+3,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        holder.memoItemDateAndSnippet.setText(das);
-        if(holder.memoItemKeywordPart.getChildCount()==0){
-            Context context = holder.memoItemKeywordPart.getContext();
-            Log.i(TAG, "onBindViewHolder: "+memoModel.getKeywords().size()+"--"+holder.memoItemKeywordPart.getChildCount());
-            for (String keyword:memoModel.getKeywords()) {
-                TextView textView = new TextView(context);
-                textView.setText(keyword);
-                textView.setTextSize(12);
-                textView.setTextColor(Color.parseColor("#FFBA69"));
-                textView.setBackgroundColor(Color.parseColor("#eeeeee"));
-                textView.setPadding(24,4,24,14);
-                holder.memoItemKeywordPart.addView(textView);
-                TextView margin = new TextView(context);
-                margin.setText("    ");
-                holder.memoItemKeywordPart.addView(margin);
-            }
-        }
+        holder.memoItemName.setText(memoModel.getName());
+        holder.memoItemSnippet.setText(memoModel.getSnippet());
+        holder.memoItemDate.setText(formatDate(memoModel.getDate()));
+        holder.memoItemType.setText("["+memoModel.getType()+"]");
+        holder.memoItemKeywords.setText(geneKeywords(memoModel.getKeywords()));
+//        if(holder.memoItemKeywordPart.getChildCount()==0){
+//            Context context = holder.memoItemKeywordPart.getContext();
+//            Log.i(TAG, "onBindViewHolder: "+memoModel.getKeywords().size()+"--"+holder.memoItemKeywordPart.getChildCount());
+//            for (String keyword:memoModel.getKeywords()) {
+//                TextView textView = new TextView(context);
+//                textView.setText(keyword);
+//                textView.setTextSize(12);
+//                textView.setTextColor(Color.parseColor("#FFBA69"));
+//                textView.setBackgroundColor(Color.parseColor("#eeeeee"));
+//                textView.setPadding(24,4,24,14);
+//                holder.memoItemKeywordPart.addView(textView);
+//                TextView margin = new TextView(context);
+//                margin.setText("    ");
+//                holder.memoItemKeywordPart.addView(margin);
+//            }
+//        }
         //将position保存在itemView的Tag中，以便点击时进行获取
         holder.itemView.setTag(position);
     }
@@ -105,14 +123,18 @@ public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.ViewHolder> im
     //自定义的ViewHolder，持有每个Item的的所有界面元素
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView memoItemName;
-        TextView memoItemDateAndSnippet;
-        LinearLayout memoItemKeywordPart;
+        TextView memoItemType;
+        TextView memoItemSnippet;
+        TextView memoItemDate;
+        TextView memoItemKeywords;
 
         public ViewHolder(View itemView) {
             super(itemView);
             memoItemName = (TextView)itemView.findViewById(R.id.fragment_memo_item_name);
-            memoItemDateAndSnippet = (TextView)itemView.findViewById(R.id.fragment_memo_item_date_and_snippet);
-            memoItemKeywordPart = (LinearLayout)itemView.findViewById(R.id.fragment_memo_item_keyword_part);
+            memoItemType = (TextView)itemView.findViewById(R.id.fragment_memo_item_type);
+            memoItemSnippet = (TextView)itemView.findViewById(R.id.fragment_memo_item_snippet);
+            memoItemDate = (TextView)itemView.findViewById(R.id.fragment_memo_item_date);
+            memoItemKeywords = (TextView)itemView.findViewById(R.id.fragment_memo_item_keywords);
         }
     }
 }
